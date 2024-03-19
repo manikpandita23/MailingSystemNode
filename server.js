2f9c457b6105ebd277f972878fce48f0e5b0d199
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-20 01:11:27 +0530

    Fix build on port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT ||3000;
 app.use(express.json());
 
 
-app.use('/api', appRoute)
+app.use('/api', appRoute);
 
 app.listen(PORT, ()=> {
     console.log('Server initiated')
