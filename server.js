a5fdccd34ac7289fd0c85e29a5e7d75e50baf6a4
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-20 01:38:34 +0530

    Format base

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const appRoute = require('./routes/route.js')
-const app  = express();
-const PORT = process.env.PORT ||3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 
 app.use('/api', appRoute);
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:${PORT}')
 })
