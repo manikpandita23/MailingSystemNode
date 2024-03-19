4025c64fd1565983bf4a484f290221004e6749ee
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-20 01:15:54 +0530

    Port updates

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use('/api', appRoute);
 
 app.listen(PORT, ()=> {
-    console.log('Server initiated')
+    console.log('Server is running on http://localhost:${PORT}')
 })
