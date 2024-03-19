84138a5319b2104588aac1ab713c85ca03f1acaf
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-19 20:55:59 +0530

    Port initialized

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app  = express();
+const PORT = process.env.PORT ||3000;
 
 
-app.listen(3000, ()=> {
+app.listen(PORT, ()=> {
     console.log('Server initiated')
 })
