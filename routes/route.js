19efa0f4aa3341bc8ce087e6bca7a0c527bf547b
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-19 21:13:35 +0530

    Endpoint for signup

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
+const {signup} = require('../controller/appController.js')
 
 
 
-router.post('/user/signup', (req,res)=>{})
+router.post('/user/signup', signup)
