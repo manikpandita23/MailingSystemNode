e3c98602ecd0363dccad977123253a3591781caf
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-19 21:15:32 +0530

    Added getBill endpoint

--- controller/appController.js
+++ controller/appController.js
@@ -4,6 +4,10 @@ const signup = (req,res)=>{
     res.status(201).json("Signup Successful");
 }
 
+const getbill = (req,res)=>{
+    res.status(201).json("getBill Successful");
+}
+
 module.exports = {
     signup
 }
