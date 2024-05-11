1a05402eaa76ff9798eee05a0ce677e137100d64
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-05-11 23:38:59 +0530

    Initial commit

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 
 const { signup, getbill } = require('../controller/appController.js')
 
-
-
 router.post('/user/signup', signup);
 router.post('/product/getbill', getbill);
 
